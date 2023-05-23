a00abb9d0795fcae37a83bebf77dbfa872ea2ffe
Thomas More <tmore1@gmx.com>
2023-05-23 15:19:58 -0400

    Optimize 2015/19b

--- 2015/19c.py
+++ 2015/19c.py
@@ -1,5 +1,7 @@
 # https://adventofcode.com/2015/day/19#part2
 # needed help from https://eddmann.com/posts/advent-of-code-2015-day-19-medicine-for-rudolph/
+# this is an optimized version of 19b.py - see this thread for background:
+# https://old.reddit.com/r/adventofcode/comments/13lfxxq/2015_day_19_part_1_2_python_deterministic_solution/
 
 import sys
 
@@ -9,7 +11,9 @@ for line in sys.stdin:
         break
     words = line.split()
     replacements.append((words[0], words[2]))
-molecules = {sys.stdin.readline().strip(): 0}
+molecules = {sys.stdin.readline().strip()}
+molecules_seen = set()
+steps = 1
 while True:
     molecule = min(molecules, key=len)
     for replacement in replacements:
@@ -20,7 +24,10 @@ while True:
             else:
                 new_molecule = molecule[:i] + replacement[0] + molecule[i + len(replacement[1]):]
                 if new_molecule == 'e':
-                    print(molecules[molecule] + 1)
+                    print(steps)
                     exit()
-                molecules[new_molecule] = molecules[molecule] + 1
-    molecules.pop(molecule)
+                if molecule not in molecules_seen:
+                    molecules.add(new_molecule)
+    molecules.remove(molecule)
+    molecules_seen.add(molecule)
+    steps += 1
