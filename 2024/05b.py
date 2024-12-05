d624249eba2e2c38d806a839a4b009839949fcd3
Thomas More <tmore1@gmx.com>
2024-12-05 16:56:37 -0500

    Optimize 2024/05p2

--- 2024/05b.py
+++ 2024/05b.py
@@ -2,22 +2,14 @@
 
 from functools import cmp_to_key
 
-def cmp(a, b):
-    for r in rules:
-        if (r[0], r[1]) == (a, b):
-            return -1
-        if (r[1], r[0]) == (a, b):
-            return 1
-    return 0
-
-total, rules = 0, []
+total, rules = 0, set()
 for line in open(0):
     line = line.strip()
     if '|' in line:
-        rules.append(line.split('|'))
+        rules.add(tuple(line.split('|')))
     elif line != '':
         line = line.split(',')
-        line_sorted = sorted(line, key=cmp_to_key(cmp))
+        line_sorted = sorted(line, key=cmp_to_key(lambda x, y: -1 if (x, y) in rules else 1 if (y, x) in rules else 0))
         if line != line_sorted:
             total += int(line_sorted[len(line) // 2])
 print(total)
